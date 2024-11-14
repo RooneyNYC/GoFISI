98752d7a4dd1d227368db6f7d80d7df169ae7170
unknown <IT-User@LIP1-ITUM-34.CL1C.daa>
2024-11-14 13:46:09 +0100

    init

--- assets/layout/header.php
+++ assets/layout/header.php
@@ -44,7 +44,7 @@
       <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
           <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">Daniel@Gerking.de</a></i>
-          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+49 190 666 666</span></i>
+          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+49 231 - 90 20 0</span></i>
         </div>
         <div class="social-links d-none d-md-flex align-items-center">
          
